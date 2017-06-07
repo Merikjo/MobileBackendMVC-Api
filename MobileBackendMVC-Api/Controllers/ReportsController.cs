4c0f5f4a71feb86f726f86331b6e28a7fe761872
Johanna Merikanto <johanna.merikanto@stud.pointcollege.fi>
2017-06-07 10:12:19 +0300

    Tallennetut muutokset.

--- MobileBackendMVC-Api/Controllers/ReportsController.cs
+++ MobileBackendMVC-Api/Controllers/ReportsController.cs
@@ -148,6 +148,43 @@ namespace MobileBackendMVC_Api.Controllers
 
             return Json(model, JsonRequestBehavior.AllowGet);
         }
-         
+
+
+        public ActionResult GetTimesheetCounts2(string onlyComplete)
+        {
+            ReportChartDataViewModel model = new ReportChartDataViewModel();
+
+            JohaMeriSQL2Entities entities = new JohaMeriSQL2Entities();
+            try
+            {
+                model.Labels = (from wa in entities.WorkAssignments
+                                orderby wa.Id_WorkAssignment
+                                select wa.Title).ToArray();
+
+                if (onlyComplete == "1")
+                {
+                    model.Counts = (from ts in entities.Timesheets
+                                    where (ts.WorkComplete == true)
+                                    orderby ts.Id_WorkAssignment
+                                    group ts by ts.Id_WorkAssignment into grp
+                                    select grp.Count()).ToArray();
+                }
+                else
+                {
+                    model.Counts = (from ts in entities.Timesheets
+                                    orderby ts.Id_WorkAssignment
+                                    group ts by ts.Id_WorkAssignment into grp
+                                    select grp.Count()).ToArray();
+                }
+
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
+
+    }
     }
